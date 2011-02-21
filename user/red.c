4318d3c4b637823bc4481afb8fc10f8fac238650
Eric Enright <erenright@protonmail.ch>
2011-02-21 05:16:31 +0000

    Use a recurring task to pulse the red LED faster every 10 seconds.
    git-svn-id: file:///nextcloud/data/eric/files/src/svn/ertos/trunk@748 a59facc2-77eb-43ea-bc74-3b41278e58d5

--- user/red.c
+++ user/red.c
@@ -9,9 +9,26 @@
 #include <stdio.h>
 #include <sleep.h>
 
+static volatile uint8_t *leds = (uint8_t *)LEDS;
+
+static void handle_alarm(void)
+{
+	int i;
+
+	for (i = 0; i < 10; ++i) {
+		*leds ^= LED_RED;
+		sleep(10);
+	}
+}
+
 void red_task(void)
 {
-	volatile uint8_t *leds = (uint8_t *)LEDS;
+	struct alarm a;
+
+	a.oneshot = 0;
+	a.msec = 10000;
+	a.handler = handle_alarm;
+	alarm(&a);
 
 	*leds &= ~LED_RED;
 
