e21a09e2e797d3a4068b6a3ca712991a457e88e5
Eric Enright <erenright@protonmail.ch>
2011-03-06 20:02:42 +0000

    Add MII header.
    git-svn-id: file:///nextcloud/data/eric/files/src/svn/ertos/trunk@769 a59facc2-77eb-43ea-bc74-3b41278e58d5

---
+++ include/sys/mii.h
@@ -0,0 +1,55 @@
+/*
+ * include/sys/mii.h
+ *
+ * Media Independant Interface definitions.
+ */
+
+#ifndef _SYS_MII_H
+#define _SYS_MII_H
+
+// MII management register set, per IEEE 802.3 table 22-6.
+#define MII_CONTROL		0	// Control
+#define MII_STATUS		1	// Status
+#define MII_PHYID1		2	// PHY Identifier 1
+#define MII_PHYID2		3	// PHY Identifier 2
+#define MII_AUTONG_ADV		4	// Auto-Negotiation Advertisement
+#define MII_AUTONG_LPBPA	5	// Auto-Negotiation Link Partner Base
+					//	Page Ability
+#define MII_AUTONG_EXP		6	// Auto-Negotiation Expansion
+#define MII_AUTONG_NPT		7	// Auto-Negotiation Next Page Transmit
+#define MII_AUTONG_LPRNP	8	// Auto-Negotiation Link Partner
+					//	Received Next Page
+#define MII_MS_CONTROL		9	// MASTER-SLAVE Control
+#define MII_MS_STATUS		10	// MASTER-SLAVE Status
+#define MII_PSE_CONTROL		11	// PSE Control
+#define MII_PSE_STATUS		12	// PSE Status
+#define MII_MMD_ACC_CONTROL	13	// MMD Access Control
+#define MII_MMD_ACC_DATA	14	// MMD Access Address Data Register
+#define MII_XSTATUS		15	// Extended Status
+
+// MII_CONTROL bit definitions
+#define MII_CONTROL_RESET	(1<<15)	// Reset
+#define MII_CONTROL_LOOPBACK	(1<<14)	// Loopback
+
+#define MII_CONTROL_SPEED	((1<<13) | (1<<6))	// Speed Selection
+#define MII_CONTROL_SPEED_1000	(1<<6)	// 1000 Mb/s
+#define MII_CONTROL_SPEED_100	(1<<13)	// 100 Mb/s
+#define MII_CONTROL_SPEED_10	(0)	// 10 Mb/s
+
+#define MII_CONTROL_AUTONGEN	(1<<12)	// Auto-Negotiation Enable
+#define MII_CONTROL_POWERDOWN	(1<<11)	// Power Down
+#define MII_CONTROL_ISOLATE	(1<<10)	// Isolate
+#define MII_CONTROL_RESTART_AUTONG	(1<<9)	// Restart Auto-Negotiation
+
+#define MII_CONTROL_DUPLEX	(1<<8)	// Duplex Mode
+#define MII_CONTROL_DUPLEX_FULL	(1)	// Full duplex
+#define MII_CONTROL_DUPLEX_HALF	(0)	// Half duplex
+
+#define MII_CONTROL_UNIDIR	(1<<5)	// Unidirectional enable
+
+// MII_STATUS bit definitions
+#define MII_STATUS_AUTONG	(1<<5)	// Auto-Negotiation Complete
+#define MII_STATUS_CAN_AUTONG	(1<<3)	// Auto-Negotiation Ability
+#define MII_STATUS_LINK		(1<<2)	// Link Status
+
+#endif /* !_SYS_MII_H */
